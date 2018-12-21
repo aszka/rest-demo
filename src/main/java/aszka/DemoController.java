424a42da8b06213557aa6ee5871384d980da8a17
aszka <ania_jz@o2.pl>
2018-12-21 14:38:00 +0100

    Allow CORS

--- src/main/java/aszka/DemoController.java
+++ src/main/java/aszka/DemoController.java
@@ -3,6 +3,7 @@ package aszka;
 import aszka.dao.TeamDao;
 import aszka.domain.Team;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class DemoController {
 
 	@Autowired
